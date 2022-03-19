f034dbb647d894216564643b7171bdd6e8b02154
Bartłomiej Kordek <bartekordek@gmail.com>
2022-03-19 18:31:04 +0100

    WIP

---
+++ TowerDefense/Source/TowerDefense/Private/PlaceableCPP.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlaceableCPP.h"
+
+// Sets default values
+APlaceableCPP::APlaceableCPP()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlaceableCPP::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlaceableCPP::AddTurret()
+{
+
+}
+
+// Called every frame
+void APlaceableCPP::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
